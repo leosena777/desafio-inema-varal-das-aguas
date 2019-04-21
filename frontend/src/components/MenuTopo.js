408dd8ff16f184dda9f3622fdec0234732891ba3
leosena777 <leo.sena777@gmail.com>
2019-04-21 12:05:18 -0300

    :art: grid aplicado no menu topo
    Aplicando grid corretamente no meu topo

--- frontend/src/components/MenuTopo.js
+++ frontend/src/components/MenuTopo.js
@@ -3,27 +3,31 @@ import React from "react";
 const MenuTopo = () => (
   <div className="menu-topo clearfix">
     <div className="container">
-      <nav>
-        <ul className="list-unstyled m-0">
-          <li>
-            <a href="#" className="active">
-              INÍCIO
-            </a>
-          </li>
-          <li>
-            <a href="#"> O CONCURSO </a>
-          </li>
-          <li>
-            <a href="#"> SOBRE </a>
-          </li>
-          <li>
-            <a href="#"> REGRAS </a>
-          </li>
-          <li>
-            <a href="#"> INSCRIÇÃO </a>
-          </li>
-        </ul>
-      </nav>
+      <div className="row">
+        <div className="col-md-10 offset-md-1">
+          <nav>
+            <ul className="list-unstyled m-0">
+              <li>
+                <a href="#" className="active">
+                  INÍCIO
+                </a>
+              </li>
+              <li>
+                <a href="#"> O CONCURSO </a>
+              </li>
+              <li>
+                <a href="#"> SOBRE </a>
+              </li>
+              <li>
+                <a href="#"> REGRAS </a>
+              </li>
+              <li>
+                <a href="#"> INSCRIÇÃO </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   </div>
 );
