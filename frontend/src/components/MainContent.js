8675e3847850915f1e1a77c4dec2141ab82e137b
leosena777 <leo.sena777@gmail.com>
2019-04-22 01:09:18 -0300

    :art: insere modal
    > modal inserido
    > otimização de layout
    > novo React.componente BoxInfo

--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -1,21 +1,195 @@
 import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-const Boxinfo = () => (
-  <div className="box-info">
-    <div className="box-blue">
-      <div className="titulo">EXPOSIÇÃO DE FOTOS </div>
-      <div className="texto">
-        20 á 24/03/2017 na divisórias da Ouvidoria (em frente ao elevador)
-      </div>
+function modalInscricao(e) {
+  e.preventDefault();
+  console.log("The link was clicked.");
+}
 
-      <div className="titulo mt-5">APRESENTAÇÃO</div>
-      <div className="texto">20/03/2017 das 16:30h ás 17:30h.</div>
-    </div>
-    <div className="box-bt">
-      <button className="mt-5">FAÇA SUA INSCRIÇÃO</button>
-    </div>
-  </div>
-);
+class Boxinfo extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    return (
+      <>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Cadastro</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form method="post" action="http://localhost:3001">
+              <Row>
+                <Col>
+                  <Form.Group controlId="formNome">
+                    <Form.Label>Nome do colaborador:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="nome"
+                      placeholder="Insira seu nome"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group controlId="formNomeFoto">
+                    <Form.Label>Nome da foto</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="nomefoto"
+                      placeholder="Insira o nome da foto"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col>
+                  <Form.Group controlId="formNome">
+                    <Form.Label>Local da foto:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="local"
+                      placeholder="Insira o local da foto"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group controlId="formNomeFoto">
+                    <Form.Label>data de registro</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="nomefoto"
+                      placeholder="MM/DD/YYYY"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col>
+                  <Form.Group controlId="formArquivoFoto">
+                    <Form.Label>Anexar Foto</Form.Label>
+                    <Form.Control
+                      type="file"
+                      name="nomefoto"
+                      placeholder="MM/DD/YYYY"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col>
+                  <Form.Group controlId="formNome">
+                    <Form.Label>Termos:</Form.Label>
+                    <div className="termos">
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Integer sollicitudin, lorem ut molestie commodo, nisi
+                        neque hendrerit arcu, a luctus velit risus vitae dui.
+                        Integer volutpat tellus id scelerisque pharetra. Class
+                        aptent taciti sociosqu ad litora torquent per conubia
+                        nostra, per inceptos himenaeos. Aliquam faucibus
+                        consequat neque, vitae vestibulum purus tincidunt ut.
+                        Aenean nec pharetra elit. Praesent a urna tortor. Nullam
+                        feugiat dignissim neque, vel ullamcorper eros. Sed
+                        tincidunt, justo vel consequat fringilla, purus sem
+                        suscipit mi, a interdum neque risus vel dolor. Fusce
+                        dignissim ac est id tincidunt. Suspendisse non ex a
+                        augue maximus suscipit eget in mauris. Nulla pulvinar
+                        lectus dolor, id imperdiet sapien facilisis sed. Aliquam
+                        erat volutpat. Duis sodales porttitor dolor non rhoncus.
+                        Proin pharetra imperdiet diam, quis iaculis eros
+                        interdum in. Etiam malesuada consequat tortor tempor
+                        varius. Integer commodo ante eget est mollis, sed
+                        malesuada felis interdum.
+                      </p>
+
+                      <p>
+                        Praesent nec tellus faucibus, venenatis neque vel,
+                        ullamcorper dui. Nunc pulvinar dapibus erat eget
+                        sagittis. Suspendisse sed neque in eros suscipit
+                        volutpat vitae in nisl. Donec quis metus ullamcorper,
+                        fringilla dolor id, aliquet libero. Vivamus nec ipsum
+                        sagittis, mollis augue ac, laoreet nisl. Donec ornare
+                        congue eros sit amet vestibulum. Curabitur ac odio
+                        turpis. Quisque commodo massa quis egestas aliquet.
+                      </p>
+
+                      <p>
+                        Sed sed venenatis arcu. Donec molestie dolor et magna
+                        ultrices consectetur. Interdum et malesuada fames ac
+                        ante ipsum primis in faucibus. Ut placerat metus nec
+                        facilisis vestibulum. Praesent non odio sit amet ante
+                        semper consectetur eu in turpis. Etiam quis nibh quam.
+                        Donec mi ex, venenatis eget sem quis, iaculis rutrum
+                        ligula. Ut lacinia aliquam quam iaculis euismod. Aliquam
+                        erat volutpat. Proin sit amet elementum magna, tincidunt
+                        laoreet ipsum. Maecenas venenatis turpis et vehicula
+                        facilisis. Phasellus aliquam ante non ultrices dapibus.
+                        Ut vel sollicitudin nibh.
+                      </p>
+                    </div>
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Form.Group controlId="formBasicChecbox">
+                <Form.Check
+                  type="checkbox"
+                  label="Eu li e concordo com o s termos"
+                />
+              </Form.Group>
+
+              <Modal.Footer className="p-0 pt-3">
+                <Button variant="primary" type="submit">
+                  Enviar
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal.Body>
+        </Modal>
+
+        <div className="box-info">
+          <div className="box-blue">
+            <div className="titulo">EXPOSIÇÃO DE FOTOS </div>
+            <div className="texto">
+              20 á 24/03/2017 na divisórias da Ouvidoria (em frente ao elevador)
+            </div>
+
+            <div className="titulo mt-5">APRESENTAÇÃO</div>
+            <div className="texto">20/03/2017 das 16:30h ás 17:30h.</div>
+          </div>
+          <div className="box-bt">
+            <button className="mt-5" onClick={this.handleShow}>
+              FAÇA SUA INSCRIÇÃO
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 const MainContent = () => (
   <main className="pb-5">
