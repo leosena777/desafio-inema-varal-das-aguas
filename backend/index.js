689c9d48272807db93e64b35e413015ee0613199
leosena777 <leo.sena777@gmail.com>
2019-04-22 12:35:00 -0300

    Resolvendo problema cross origin
    > problema com a requiscao corrigido.

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+var cors = require('cors');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({  extended:false  }));
 app.use(bodyParser.json());
 app.use(expressValidator());
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions));
+
 
 consign().include('routes').include('utils').into(app);
 
